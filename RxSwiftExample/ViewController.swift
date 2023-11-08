5833b72ac9c9d73a0d1fbb40f98e11e9576db170
David Yoon <qqpo12@naver.com>
2023-11-08 12:37:31 +0900

    Set example deafult skeleton
    * Set the default skeleton
    * Added a textfield and a submitButton
    * Removed the main storyboard
    * Added a snapkit and a rxswift

--- RxSwiftExample/ViewController.swift
+++ RxSwiftExample/ViewController.swift
@@ -6,14 +6,68 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.borderWidth = 1.0
+        textField.layer.cornerRadius = 4.0
+
+        return textField
+    }()
+
+    private lazy var submitButton: UIButton = {
+        let button = UIButton(type: .roundedRect)
+        button.setTitle("Submit", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 1.0
+        button.layer.cornerRadius = 4.0
+
+        return button
+    }()
+
+    private var disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
     }
+}
 
+private extension ViewController {
+    func setupViews() {
 
+        view.backgroundColor = .systemBackground
+
+        [
+            textField,
+            submitButton
+        ]
+            .forEach {
+                view.addSubview($0)
+            }
+
+        let offset: CGFloat = 16.0
+
+        textField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(offset)
+            $0.leading.equalToSuperview().offset(offset)
+            $0.trailing.equalToSuperview().offset(-offset)
+            $0.height.equalTo(50.0)
+        }
+
+        submitButton.snp.makeConstraints {
+            $0.top.equalTo(textField.snp.bottom).offset(offset)
+            $0.leading.equalToSuperview().offset(offset)
+            $0.trailing.equalToSuperview().offset(-offset)
+            $0.height.equalTo(50.0)
+        }
+    }
 }
 
